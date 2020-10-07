d11fd5430d0944d6d28d7c62c40f1fafc8616dc3
Fedorov-Ilya <4464606i@gmail.com>
2020-10-07 13:55:11 +0300

    add_start

---
+++ Start/src/main/java/com/example/Start/Greeting.java
@@ -0,0 +1,22 @@
+package com.example.Start;
+
+public class Greeting {
+
+    public int id;
+    private String content;
+
+    public Greeting(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public int getId(int i) {
+        return id;
+    }
+
+    public String getContent(String asdasd) {
+        return content;
+    }
+}
+
+
