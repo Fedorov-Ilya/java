d6f4572ddda89bcc2fa6fb98e7d7e20402ad277b
Fedorov-Ilya <4464606i@gmail.com>
2020-10-07 16:44:04 +0300

    add_utils

--- Start/src/main/java/com/example/Start/PostmanController.java
+++ Start/src/main/java/com/example/Start/PostmanController.java
@@ -1,32 +1,22 @@
 package com.example.Start;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 @RestController
 public class PostmanController {
+    @Autowired
+    Utils utils;
 
     @PostMapping("/greeting")
     @ResponseStatus(HttpStatus.CREATED)
     public String create(@RequestBody(required = false) String body) throws Exception {
-        String changeBody = body;
-        System.out.println(changeBody);
-        Pattern pattern = Pattern.compile("(?<=(<Type>))(.*)(?=(</Type>))");
-        Matcher matcher = pattern.matcher(changeBody);
-
-        if (matcher.find()) {
-            System.out.println(matcher.group(0));
-        } else {
-            System.out.println("TEXT NOT FOUND");
-        }
-
+        System.out.println(utils.getSubstring(body,"<Type>","</Type>"));
         return body;
     }
 }
