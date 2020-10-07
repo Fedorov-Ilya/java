51443503961d51ee7bc8d4b42e4485e662c4b6c7
Fedorov-Ilya <4464606i@gmail.com>
2020-10-07 14:46:42 +0300

    add_greetingController_2

---
+++ Start/src/main/java/com/example/Start/GreetingController_2.java
@@ -0,0 +1,28 @@
+package com.example.Start;
+
+import com.google.gson.Gson;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@RestController
+public class GreetingController_2 {
+
+    private static final Logger logger = LogManager.getLogger(GreetingController.class);
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @PostMapping("/greeting")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody(required = false) String body) throws Exception {
+        System.out.println(body);
+        return rbody;
+    }
+}
